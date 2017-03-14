e9c21ee9d0bffce3b287dcfc96647be3504f707f
impersin <closer124@gmail.com>
2017-03-14 16:43:42 -0700

    Add favicon link

--- server/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./server.js');
-const port = 3001;
-
-app.listen(port, ()=>{
-  console.log('listening port', port);
-});
--- server/server.js
+++ server/server.js
@@ -9,5 +9,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/../client/index.html'));
 });
 
+const port = process.env.PORT || 8080;
 
-module.exports = app;
+app.listen(port, ()=>{
+  console.log('listening port', port);
+});
