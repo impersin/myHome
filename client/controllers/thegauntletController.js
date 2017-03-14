3a10fcba5b01f8840597830d1bc503c9c38129d0
impersin <closer124@gmail.com>
2017-03-14 01:44:10 -0700

    Create thegauntlet controller and create potfolio

---
+++ client/controllers/thegauntletController.js
@@ -0,0 +1,13 @@
+angular.module('myApp.thegauntlet', [])
+  .controller('thegauntletController', function($scope) {
+    $scope.portfolio =
+    {
+      title: 'Thegauntlet',
+      url: 'https://the-gauntlet2.herokuapp.com',
+      desc: 'Social media based web application that allows users to create custom competitions and vote on winner.',
+      stack: 'tglStack.png',
+      path: 'thegauntlet',
+      images: ['cover.png', 'img1.png', 'img2.png', 'img3.png', 'img4.png']
+        // ['aws.png', 'react.png', 'redux.png', 'sql.png', 'node.png', 'express.png', 'webpack.png'] 
+    };
+  });
