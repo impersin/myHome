90398f47e19e1ffbd000f7ba250194f86a3a675e
impersin <closer124@gmail.com>
2017-04-08 19:29:45 -0700

    Restore

--- server/server.js
+++ server/server.js
@@ -3,14 +3,10 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const AWS = require('aws-sdk');
-const YQL = require('yql');
-const s3 = new AWS.S3();
+// const YQL = require('yql');
 const isDeveloping = process.env.NODE_ENV !== 'production';
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());  
-app.use(express.static(path.join(__dirname, '/../client')));
-
+// const option = AWS.config.loadFromPath('./config.json');
+// const option = require('./config.json');
 if (!isDeveloping) {
   AWS.config.update({
     accessKeyId: process.env.S3_KEY, 
@@ -19,15 +15,19 @@ if (!isDeveloping) {
   });
 }
 
+var s3 = new AWS.S3();
+
+var params = { 
+  Bucket: 'babymomo',
+  Delimiter: '/',
+};
+  
+app.use(express.static(path.join(__dirname, '/../client')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/../client/index.html'));
 });
 
-const params = { 
-  Bucket: 'babymomo',
-  Delimiter: '/',
-};
 app.get('/api/images', (req, res)=>{
   s3.listObjects(params, function (err, data) {
     if (err) { throw err; }
@@ -35,14 +35,14 @@ app.get('/api/images', (req, res)=>{
   });
 });
 
-let query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text in ("san francisco, ca", "pleasanton, ca", "dublin, ca", "palo alto, ca", "san mateo, ca"))');
+// let query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text in ("san francisco, ca", "pleasanton, ca", "dublin, ca", "palo alto, ca", "san mateo, ca"))');
 
-app.get('/api/weather', (req, res)=>{
-  query.exec(function(err, data) {
-    if (err) { throw err; }
-    res.send(data);
-  }); 
-});
+// app.get('/api/weather', (req, res)=>{
+//   query.exec(function(err, data) {
+//     if (err) { throw err; }
+//     res.send(data);
+//   }); 
+// });
 
 const port = process.env.PORT || 8080;
 
